5fbb407209e7538895bfa2c1edf1298469a100ce
Tony Cha <tony.cha@gmail.com>
2020-01-20 20:18:37 -0500

    remove absolute path for polling script

--- poll_rig.sh
+++ poll_rig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set +x
-cd /home/pi/smart-plug-power-monitor/examples/simple
+cd examples/simple
 
 sudo node downstairs.js > /home/pi/poll_pipe &
 sleep infinity < /home/pi/poll_pipe &
